b0b2e8faf6c26940e7c23ebf1be67ef75248f968
Frank Houweling <houweling.frank@gmail.com>
2019-01-31 16:14:15 +0100

    Create Module.php

---
+++ src/Module.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of Zend Session Controller Plugin
+ * For license information, please view the LICENSE file that was distributed with this source code.
+ *
+ * Written by Frank Houweling <fhouweling@senet.nl>, 31-1-2019
+ */
+
+namespace FrankHouweling\ZendSessionControllerPlugin;
+
+
+class Module
+{
+    public function getConfig()
+    {
+        return __DIR__ . '/../config/module.config.php';
+    }
+}
